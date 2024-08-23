c52f1be60225d32865235028e59cfdd672f62e37
diptigandhi24 <gandhidipti.s@gmail.com>
2024-08-23 17:41:15 +0530

    removing the trail and error of manual suspending

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,13 @@ import { useState, Suspense } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import SuspenseComponent from "./api/makefirstrequest.jsx";
+
 function App() {
   //   const data = useFetch("https://swapi.dev/api/people");
 
   return (
     <>
-      <Suspense fallback="Loading...">
-        <SuspenseComponent />
-      </Suspense>
+      <h1>Hello Suspense</h1>
     </>
   );
 }
