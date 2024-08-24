a062c6933cbcc11bb0c912b28e46e7304730164c
diptigandhi24 <gandhidipti.s@gmail.com>
2024-08-24 09:16:42 +0530

    one more demo you cannot suspend component by initiaing fetch request inside useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, Suspense } from "react";
+import { useState, Suspense, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
@@ -7,8 +7,8 @@ const resource = {
   data: null,
   promise: null,
 };
-function useFetch() {
-  const url = "https://swapi.dev/api/people";
+async function fetchData(url) {
+  // const url = "https://swapi.dev/api/people";
 
   resource.promise = fetch(url)
     .then((res) => {
@@ -21,16 +21,33 @@ function useFetch() {
       console.log("received a data", data);
       resource.data = data;
     });
+  throw resource.promise;
+}
+function useDataFetch(url) {
+  const [data, setData] = useState(resource.data);
+
+  useEffect(() => {
+    if (!resource.promise) {
+      fetchData(url);
+    }
+
+    let promise = resource.promise;
+
+    promise
+      .then((data) => setData(data))
+      .catch((error) => {
+        //take care of the error here
+      });
+  }, [url]);
 
   if (resource.data) {
-    return resource.data;
+    return data;
   } else {
     throw resource.promise;
   }
 }
-
 function App() {
-  let data = useFetch();
+  let data = useDataFetch("https://swapi.dev/api/people");
   console.log("Data from the custom hook", data);
   return (
     <>
